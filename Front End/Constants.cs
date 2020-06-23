a0ac5ad421b0ddc721d59fd031a56580f503c965
Fola Alasi <34408186+FolaShocky@users.noreply.github.com>
2020-06-23 14:46:09 +0100

    Create Constants.cs
    Uploaded Constants.cs

---
+++ Front End/Constants.cs
@@ -0,0 +1,84 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DissertWindowsFormApplication
+{
+    class Constants
+    {
+        public const string No_Company_Exists = "No Company Exists";
+
+        public const string Sign_Up_Task = "SignUpTask";
+
+        public const string Login_Task = "LoginTask";
+
+        public const string Multiple_Company_Retrieval_Task = "MultipleCompanyRetrievalTask";
+
+        public const string Single_Company_Retrieval_Task = "SingleCompanyRetrievalTask";
+
+        public const string CBox_Company_Name_Population_Task = "CBoxCompanyNameTask";
+
+        public const string All_Admins_Retrieval_Task = "AllAdminsRetrievalTask";
+
+        public const string Application_Json = "application/json";
+        public const string User_Type_Regular_User = "RegularUser";
+                            
+        public const string User_Type_Admin = "Admin";
+
+        public const string Server_String = "http://localhost/WebApplication1";
+        public const string Db_Table_User = "tblUser";
+
+        //This constant represents the user's id
+        public const string Db_Field_Id = "Id";
+
+        public const string Db_Field_Type = "Type";
+
+        public const string Db_Entry_Regular_User = "RegularUser";
+
+        public const string Db_Entry_Admin = "Admin";
+
+        //That table in the database called 'tblUserDetails'
+        public const string Db_Table_User_Details = "tblUserDetails";
+
+        public const string Db_Field_FirstName = "FirstName";
+
+        public const string Db_Field_Surname = "Surname";
+
+        public const string Db_Field_Username = "Username";
+
+        public const string Db_Field_Password = "Password";
+
+        public const string Db_Field_Company_Name = "CompanyName";
+
+        public const string Db_Field_Date_Joined = "DateJoined";
+        //That table in the database called 'tblCompany'
+        public const string Db_Table_Company = "tblCompany";
+
+        public const string Db_Field_Company_Id = "CompanyId";
+
+
+        //That table in the database called 'tblItem'
+        public const string Db_Table_Item = "tblItem";
+
+        public const string Db_Field_Item_Id = "ItemId";
+
+        public const string Db_Field_Name = "Name";
+
+        public const string Db_Field_Quantity = "Quantity";
+
+        public const string Db_Field_Reason = "Reason";
+
+        public const string Db_Field_User_Id = "UserId";
+
+        public const string Db_Field_Is_Active = "IsActive";
+
+        public const string Db_Field_Admin_Id = "AdminId";
+
+        public const string Change_Made = "ChangeMade";
+
+        public const string New_Line = "\n";
+    
+    }
+}
